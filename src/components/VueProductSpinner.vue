908e2ee3323743fc48633e7e237cf6fd15907754
Mihai <mihai_visovan@yahoo.com>
2020-05-22 19:00:54 +0300

    Hi, I've added the animation feature that you wanted. Also, solved several bugs like: If you slide the images slowly to the left, they actually went right. Or, the event of dragging was not removed when you left the images wrapper. And the last one I've noticed was having 'wheel' event even if the prop is not true. I hope this helps!

--- src/components/VueProductSpinner.vue
+++ src/components/VueProductSpinner.vue
@@ -34,49 +34,42 @@ import PreloadImages from "../PreloadImages.js";
 
 export default {
   name: "VueProductSpinner",
-
   props: {
     images: {
       type: Array,
-      required: true
+      required: true,
     },
     infinite: {
       type: Boolean,
       required: false,
-      default: () => true
+      default: () => true,
     },
     speed: {
       type: Number,
       required: false,
-      default: () => 3
-    },
-    touchDrag: {
-      type: Boolean,
-      required: false,
-      default: () => true
+      default: () => 3,
     },
     mouseWheel: {
       type: Boolean,
       required: false,
-      default: () => true
-    },
-    mouseDrag: {
-      type: Boolean,
-      required: false,
-      default: () => true
+      default: () => false,
     },
     slider: {
       type: Boolean,
       required: false,
-      default: () => false
+      default: () => false,
     },
     sliderClass: {
       type: String,
       required: false,
-      default: () => ""
-    }
+      default: () => "",
+    },
+    animation: {
+      type: Boolean,
+      required: false,
+      default: () => false,
+    },
   },
-
   data() {
     return {
       imagesPreloaded: false,
@@ -84,18 +77,21 @@ export default {
       spinner: {
         current: 0,
         size: 0,
-        currentPath: null
+        currentPath: null,
       },
       mouse: {
-        isMoving: false
+        isMoving: false,
+        isDragging: false,
       },
       touch: {
         isMoving: false,
-        initialX: 0
-      }
+        initialX: 0,
+        isDragging: false,
+      },
+      animationRequestID: 0,
+      lastPosition: 0,
     };
   },
-
   watch: {
     images: function() {
       this.imagesPreloaded = false;
@@ -103,135 +99,162 @@ export default {
       this.initSpinner();
     }
   },
-
-  beforeMount() {
-    this.handlePreload();
-  },
-
-  mounted() {
-    this.$refs.componentContainer.addEventListener(
-      "wheel",
-      this.handleWheel,
-      false
-    );
-  },
-
-  beforeDestroy() {
-    this.$refs.componentContainer.removeEventListener(
-      "wheel",
-      this.handleWheel
-    );
-  },
-
   created() {
     this.initSpinner();
   },
-
+  beforeMount() {
+    this.handlePreload();
+  },
+  mounted() {
+    window.addEventListener(
+      'mouseup',
+      this.handleMouseDragging,
+    );
+    window.addEventListener(
+      'touchend',
+      this.handleTouchDragging,
+    );
+    if (this.mouseWheel) {
+      this.$refs.componentContainer.addEventListener(
+        'wheel',
+        this.handleWheel,
+        false,
+      );
+    }
+  },
+  beforeDestroy() {
+    if (this.mouseWheel) {
+      this.$refs.componentContainer.removeEventListener(
+        'wheel',
+        this.handleWheel,
+      );
+    }
+    window.removeEventListener(
+      'mouseup',
+      this.handleMouseDragging,
+    );
+    window.removeEventListener(
+      'touchend',
+      this.handleTouchDragging,
+    );
+  },
   methods: {
     initSpinner() {
       this.spinner.size = this.images.length;
-      this.spinner.currentPath = this.images[0];
+      if (this.animation) {
+        this.spinImages(0, this.spinner.size);
+      } else {
+        this.spinner.currentPath = this.images[0];
+      }
     },
-
     handlePreload() {
       PreloadImages(this.images).then(() => (this.imagesPreloaded = true));
     },
-
     handleKeydown(event) {
       if (event.keyCode === 39) {
         event.preventDefault();
-        this.handleMovement(1);
+        this.handleMovement(this.lastPosition + 1);
       }
       if (event.keyCode === 37) {
         event.preventDefault();
-        this.handleMovement(-1);
+        this.handleMovement(this.lastPosition - 1);
       }
     },
-
-    handleSlider(event) {
-      this.spinner.current = parseInt(event.target.value);
-      this.spinner.currentPath = this.images[event.target.value - 1];
-    },
-
-    handleMouseDown() {
-      this.mouse.isMoving = true;
-    },
-
-    handleMouseUp() {
-      this.mouse.isMoving = false;
-    },
-
-    handleMouseMove(event) {
-      if (this.mouse.isMoving && this.mouseDrag) {
-        this.handleMovement(event.movementX);
-      }
-    },
-
-    handleTouchStart(event) {
-      event.preventDefault();
-      this.touch.isMoving = true;
-      this.touch.initialX = event.touches[0].pageX;
-    },
-
-    handleTouchEnd() {
-      this.touch.isMoving = false;
-    },
-
-    handleTouchMove(event) {
-      if (this.touchDrag) {
-        const delta = -(this.touch.initialX - event.touches[0].pageX);
-        this.handleMovement(delta);
-      }
-    },
-
     handleWheel(event) {
       if (this.mouseWheel) {
         event.preventDefault();
         this.handleMovement(event.deltaY);
       }
     },
-
+    handleSlider(event) {
+      this.spinner.current = Number(event.target.value);
+      this.spinner.currentPath = this.images[event.target.value - 1];
+    },
+    handleMouseDown() {
+      if (this.animation) {
+        this.stopAnimation();
+      }
+      this.mouse.isDragging = true;
+      this.mouse.isMoving = true;
+    },
+    handleMouseUp() {
+      this.mouse.isMoving = false;
+    },
+    handleMouseMove(event) {
+      if (this.mouse.isMoving && this.mouse.isDragging) {
+        this.handleMovement(event.pageX);
+      }
+    },
+    handleMouseDragging() {
+      this.mouse.isDragging = false;
+    },
+    handleTouchDragging() {
+      this.touch.isDragging = false;
+    },
+    handleTouchStart(event) {
+      event.preventDefault();
+      if (this.animation) {
+        this.stopAnimation();
+      }
+      this.touch.isMoving = true;
+      this.touch.isDragging = true;
+      this.touch.initialX = event.touches[0].pageX;
+    },
+    handleTouchEnd() {
+      this.touch.isMoving = false;
+    },
+    handleTouchMove(event) {
+      if (this.touch.isDragging) {
+        const delta = -(this.touch.initialX - event.touches[0].pageX);
+        this.handleMovement(delta);
+      }
+    },
+    spinImages(index, lastIndex) {
+      let i = index;
+      if (i !== lastIndex) {
+        this.spinner.currentPath = this.images[i];
+        this.animationRequestID = window.requestAnimationFrame(() => this.spinImages(i, lastIndex));
+      } else {
+        this.stopAnimation();
+        [this.spinner.currentPath] = this.images;
+        return;
+      }
+      i += 1;
+    },
+    stopAnimation() {
+      if (this.animationRequestID) {
+        window.cancelAnimationFrame(this.animationRequestID);
+        this.animationRequestID = null;
+      }
+    },
     handleMovement(delta) {
-      this.speedController++;
+      this.speedController += 1;
       if (this.speedController < this.speed) {
         return;
       }
-
       if (this.speedController > this.speed) {
         this.speedController = 0;
       }
-
-      if (delta >= 0) {
-        /**
-         * User is moving forward
-         */
-        if (
-          this.spinner.current >= 0 &&
-          this.spinner.current < this.spinner.size
-        ) {
-          this.spinner.current++;
+      if (delta > this.lastPosition) {
+        if (this.spinner.current >= 0 && this.spinner.current < this.spinner.size) {
+          this.spinner.current += 1;
           this.spinner.currentPath = this.images[this.spinner.current - 1];
-        } else {
-          if (this.infinite) {
-            this.spinner.current = 1;
-            this.spinner.currentPath = this.images[this.spinner.current - 1];
-          }
+        } else if (this.infinite) {
+          this.spinner.current = 1;
+          this.spinner.currentPath = this.images[this.spinner.current - 1];
         }
-      } else {
-        /**
-         * User is moving backward
-         */
+      } else if (delta < this.lastPosition) {
         if (this.spinner.current >= 0 && this.spinner.current - 1 > 0) {
-          this.spinner.current--;
+          this.spinner.current -= 1;
           this.spinner.currentPath = this.images[this.spinner.current - 1];
-        } else {
-          if (this.infinite) {
-            this.spinner.current = this.spinner.size;
-            this.spinner.currentPath = this.images[this.spinner.current - 1];
-          }
+        } else if (this.infinite) {
+          this.spinner.current = this.spinner.size;
+          this.spinner.currentPath = this.images[this.spinner.current - 1];
         }
       }
-    }
-  }
+      this.lastPosition = delta;
+    },
+  },
 };
 </script>
+
